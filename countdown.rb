6f08c8395552714882e6b341104cb068a78a262e
speraza545 <s.peraza545@gmail.com>
2021-02-27 12:00:35 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(integer)
     number = integer
-    while number >= 0
+    while number > 0
         puts "#{number} SECOND(S)!"
         number -= 1
     end
@@ -12,7 +12,7 @@ end
 def countdown_with_sleep(integer)
     count = integer
     while count >= 0 
-        puts "#{count} SECONDS!"
+        puts "#{count} SECOND(S)!"
         count -= 1
         sleep 1
     end
